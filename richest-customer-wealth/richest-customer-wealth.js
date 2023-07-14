715a7a91f81eb6164a06b5ed1c7abbb434b0b8a6
Cuong Ha <34791382+cuongha0701@users.noreply.github.com>
2023-07-14 23:29:07 +0700

    Time: 51 ms (91.15%) | Memory: 42.1 MB (52.97%) - LeetSync

--- richest-customer-wealth/richest-customer-wealth.js
+++ richest-customer-wealth/richest-customer-wealth.js
@@ -4,13 +4,12 @@
  */
 var maximumWealth = function (accounts) {
   let max = 0;
-  let sum = 0;
   for (let i = 0; i < accounts.length; i++) {
+    let sum = 0;
     for (let j = 0; j < accounts[i].length; j++) {
       sum += accounts[i][j];
     }
     max < sum && (max = sum);
-    sum = 0;
   }
   return max;
 };
