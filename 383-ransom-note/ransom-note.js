b60950b9aa1a707fe5d1520cc289e7569bc345e2
Cuong Ha <34791382+cuongha0701@users.noreply.github.com>
2023-07-19 18:55:35 +0700

    Time: 81 ms (78.52%) | Memory: 48.7 MB (17.72%) - LeetSync

--- 383-ransom-note/ransom-note.js
+++ 383-ransom-note/ransom-note.js
@@ -4,19 +4,13 @@
  * @return {boolean}
  */
 var canConstruct = function (ransomNote, magazine) {
- if (ransomNote.length > magazine.length) return false;
-
   let tempMagazine = magazine;
   for (let i = 0; i < ransomNote.length; i++) {
-    const r = ransomNote.charAt(i);
-    for (let j = 0; j < tempMagazine.length; j++) {
-      if (ransomNote.charAt(i) === tempMagazine.charAt(j)) {
-        tempMagazine = tempMagazine.replace(tempMagazine.charAt(j), '');
-        break;
-      }
+    if (!tempMagazine.includes(ransomNote[i])) {
+      return false;
+    } else {
+      tempMagazine = tempMagazine.replace(ransomNote[i], '');
     }
   }
-  if (magazine.length - tempMagazine.length === ransomNote.length) return true;
-
-  return false;
+  return true;
 };
