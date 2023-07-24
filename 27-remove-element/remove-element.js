a92171b43698f3e1cf5ce9d382e6a1578f3f225c
Cuong Ha <34791382+cuongha0701@users.noreply.github.com>
2023-07-24 12:30:39 +0700

    Time: 64 ms (32.93%) | Memory: 41.9 MB (65.24%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let length = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[length] = nums[i];
+      length++;
+    }
+  }
+
+  return length;
+};
