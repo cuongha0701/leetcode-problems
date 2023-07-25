918e89b8b903f500e78b5d67c4b39df82bcd6738
Cuong Ha <34791382+cuongha0701@users.noreply.github.com>
2023-07-25 15:47:12 +0700

    Time: 66 ms (59.33%) | Memory: 45.2 MB (18.12%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.js
+++ 978-valid-mountain-array/valid-mountain-array.js
@@ -3,9 +3,17 @@
  * @return {boolean}
  */
 var validMountainArray = function (arr) {
+  if (
+    arr.length < 3 ||
+    arr[0] >= arr[1] ||
+    arr[arr.length - 2] <= arr[arr.length - 1]
+  ) {
+    return false;
+  }
+
   let left = 0;
   let right = arr.length - 1;
-  while (left < arr.length - 1 && arr[left] < arr[left + 1]) left++;
-  while (right > 0 && arr[right - 1] > arr[right]) right--;
-  return left > 0 && left === right && right < arr.length - 1;
+  while (arr[left] < arr[left + 1]) left++;
+  while (arr[right - 1] > arr[right]) right--;
+  return left === right;
 };
