b1b7cfc20b2dc7b04510b4de509b1f1e95e068ca
Cuong Ha <34791382+cuongha0701@users.noreply.github.com>
2023-07-31 21:38:55 +0700

    Time: 49 ms (97.53%) | Memory: 46.5 MB (7.42%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    const set = new Set(nums);
+  if (set.size < 3) return Math.max(...set);
+  for (let i = 0; i < 2; i++) set.delete(Math.max(...set));
+  return Math.max(...set);
+};
